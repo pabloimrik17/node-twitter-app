4f214215109ded40f5da00b051ba16616124647d
Etherless <pabloimrik17@gmail.com>
2017-10-22 20:14:47 +0200

    Loki database setup

--- app/server.js
+++ app/server.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
 
-var routes = require('./routes')(app);
-
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/../public/views');
 
 app.use(express.static(__dirname + '/../public'));
 
-
+require('./database');
+require('./routes')(app);
 
 module.exports = app;
